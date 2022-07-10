0ee58ee565bf87a0941fea7a186363515ae602ca
VIKRANT CHAUDHARY <VIKRANT CHAUDHARY@HP-NOTEBOOK>
2022-07-10 19:08:32 +0530

    Add project files.

---
+++ OrdersUsingDB/CustomerMaster.cs
@@ -0,0 +1,275 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Net;
+using System.Net.Mail;
+using System.Text.RegularExpressions;
+
+namespace OrdersUsingDB
+{
+    public class CustomerMaster
+    {
+
+        public static string sqlConnectionStr = @"Data Source=HP-NOTEBOOK;Initial Catalog=OrdersDB;Integrated Security=True";
+        SqlConnection Connection = new SqlConnection(sqlConnectionStr);
+        string FirstName, LastName, Email;
+        int Phone;
+
+
+        public void AddCustomer()
+        {
+
+            Console.WriteLine(" Enter Customer's First Name to Add");
+            FirstName = Console.ReadLine();
+
+            Console.WriteLine(" Enter Customer's Last Name to Add");
+            LastName = Console.ReadLine();
+
+            Console.WriteLine(" Enter Customer's Phone No to Add");
+            Phone = int.Parse(Console.ReadLine());
+
+
+        email:
+
+            Console.WriteLine(" Enter Email to Add");
+            Email = Console.ReadLine();
+
+            if (!mailValidation(Email))
+
+            {
+                Console.WriteLine("Enter Some Valid  Email");
+                goto email;
+
+            }
+            if (FirstName != "" && LastName != "" && Email != "")
+            {
+                if (!CustomerExistOrNot(Email) && mailValidation(Email))
+                {
+                    SqlConnection sqlConnection = new SqlConnection(sqlConnectionStr);
+                    string sql = "insert into CustomerMaster values('" + FirstName + "','" + LastName + "', " + Phone + ", '" + Email + "')";
+                    SqlDataAdapter adapter = new SqlDataAdapter(sql, Connection);
+                    DataTable dataTable = new DataTable();
+                    adapter.Fill(dataTable);
+                    Console.WriteLine("Added Succesfully");
+
+                    emailsend(Email, FirstName, LastName);
+
+
+
+
+                }
+                else
+                {
+                    Console.WriteLine("Please enter a Valid Email..it Already Exits");
+                }
+
+
+            }
+
+
+            else
+            {
+                Console.WriteLine("Please enter  Email..It can't be null");
+            }
+
+
+
+
+
+        }
+
+
+        public void UpdateCustomer()
+        {
+        Update:
+
+            Console.WriteLine(" Enter Customer's First Name to Add");
+            FirstName = Console.ReadLine();
+
+            Console.WriteLine(" Enter Customer's Last Name to Add");
+            LastName = Console.ReadLine();
+
+            Console.WriteLine(" Enter Customer's Phone No to Add");
+            Phone = int.Parse(Console.ReadLine());
+
+
+        email1:
+
+            Console.WriteLine(" Enter Email to Add");
+            Email = Console.ReadLine();
+
+            if (!mailValidation(Email))
+
+            {
+                Console.WriteLine("Enter Some   Email..It can not be null");
+                goto email1;
+
+            }
+            if (Email != "")
+            {
+                if (!CustomerExistOrNot(Email) && mailValidation(Email))
+                {
+                    SqlConnection sqlConnection = new SqlConnection(sqlConnectionStr);
+                    string sql = "update ItemMaster set Firstname='" + FirstName + "' ,  Lastname='" + LastName + "' , Phone=" + Phone + "Email= '" + Email + "' ";
+                    SqlDataAdapter adapter = new SqlDataAdapter(sql, Connection);
+                    DataTable dataTable = new DataTable();
+                    adapter.Fill(dataTable);
+                    Console.WriteLine("Updated Succesfully");
+
+
+                }
+                else
+                {
+                    Console.WriteLine("Please enter a Valid Email..it Already Exits");
+                    goto Update;
+                }
+
+
+            }
+
+
+            else
+            {
+                Console.WriteLine("Please enter  Email..It can't be null");
+                goto Update;
+            }
+
+
+
+
+
+        }
+
+        public void DeleteCustomer()
+        {
+        top2:
+
+            Console.WriteLine("Enter the Email To be Delete");
+            string Email = Console.ReadLine();
+
+
+
+            if (Email == "")
+
+            {
+                Console.WriteLine("Enter Some Email..");
+                goto top2;
+
+            }
+            else
+            {
+                if (CustomerExistOrNot(Email))
+                {
+                    SqlConnection sqlConnection = new SqlConnection(sqlConnectionStr);//connection establishment
+                    string sql = "delete from CustomerMaster where Email='" + Email + "'";
+                    SqlDataAdapter adp = new SqlDataAdapter(sql, Connection);
+                    DataTable table = new DataTable();
+                    adp.Fill(table);
+
+                    Console.WriteLine();
+                    Console.WriteLine();
+
+
+                    Console.WriteLine("Deleted Succesfully");
+
+
+                }
+                else
+                {
+                    Console.WriteLine("Please enter a Valid Email to delete");
+                    goto top2;
+                }
+
+
+
+            }
+        }
+
+        public DataTable ListAllCustomers()
+        {
+            SqlConnection sqlConnection = new SqlConnection(sqlConnectionStr);//connection establishment
+            string db = sqlConnection.Database;
+            string sql = "select * from CustomerMaster";
+            SqlDataAdapter adp = new SqlDataAdapter(sql, Connection);
+            DataTable dataTable = new DataTable();
+            adp.Fill(dataTable);
+            Console.WriteLine("FirstName\tLastname\tPhone\t\tEmail");
+            for (int i = 0; i < dataTable.Rows.Count; i++)
+            {
+                for (int j = 0; j < dataTable.Columns.Count; j++)
+                {
+                    Console.Write(dataTable.Rows[i][j] + "\t\t");
+                }
+                Console.WriteLine();
+            }
+            return dataTable;
+
+
+        }
+
+        public bool mailValidation(string Email)
+        {
+
+            Regex eml = new Regex(@"^[a-zA-Z]+[._]{0,1}[0-9a-zA-Z]+[@][a-zA-Z]+[.][a-zA-Z]{2,3}([.]+[a-zA-Z]{2,3}){0,1}");
+            Match m = eml.Match(Email);
+            if (m.Success)
+            {
+                return true;
+
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+        public bool CustomerExistOrNot(string Email)
+        {
+            DataTable dataTable = new DataTable();
+            string sql = "select * from CustomerMaster where Email='" + Email + "'";
+            SqlDataAdapter adapter = new SqlDataAdapter(sql, Connection);
+            adapter.Fill(dataTable);
+            if (dataTable.Rows.Count > 0)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        void emailsend(string to, string firstname, string lastname)
+        {
+            string from = "gopal.banger@kelltontech.com";
+            string password = "123";
+            string subject = "Welcome Dear Customer";
+            string body = "<h1>Dear, " + firstname + " " + lastname + " </h1>\nThanks for registering with us..Have a good day";
+            try
+            {
+                MailMessage message = new MailMessage();
+                SmtpClient smtp = new SmtpClient();
+                message.From = new MailAddress(from);
+                message.To.Add(new MailAddress(to));
+                message.Subject = subject;
+                message.IsBodyHtml = true; //to make message body as html  
+                message.Body = body;
+                smtp.Port = 587;
+                smtp.Host = "smtp.gmail.com"; //for gmail host  
+                smtp.EnableSsl = true;
+                smtp.UseDefaultCredentials = false;
+                smtp.Credentials = new NetworkCredential(from, password);
+                smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
+                smtp.Send(message);
+                Console.WriteLine("Mail Sent Succesfully");
+
+
+
+
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+    }
+}
---
+++ OrdersUsingDB/Program.cs
@@ -0,0 +1,202 @@
+﻿using OrderUsingDB;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrdersUsingDB
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("################ Welcome to Order Assignment ################");
+            Console.WriteLine("Please Select One of the Options:\n");
+            Console.WriteLine("\t\tPress 1 for Item Master");
+            Console.WriteLine("\t\tPress 2 for Customer Master");
+            Console.WriteLine("\t\tPress 3 to Exit");
+            int choice1 = Convert.ToInt32(Console.ReadLine());
+            switch (choice1)
+            {
+
+
+                case 1:
+
+                Portal:
+
+                    ItemMaster itemMaster = new ItemMaster();
+
+                    Console.WriteLine("****************************************Welcome to Item Portal************************************");
+                    Console.WriteLine("Please Select one of the Options :\n");
+                    Console.WriteLine("\t\tPress 1 to Insert Item Data.");
+                    Console.WriteLine("\t\tPress 2 to Update Item Data.");
+                    Console.WriteLine("\t\tPress 3 to Delete Item.");
+                    Console.WriteLine("\t\tPress 4 to all item Table data.");
+                    Console.WriteLine("\t\tPress 5 to Exit");
+
+                    int choice2 = Convert.ToInt32(Console.ReadLine());
+                    switch (choice2)
+                    {
+                        case 1:
+                            itemMaster.AddItem();
+                            goto Portal;
+                        case 2:
+                            itemMaster.DeleteItem();
+                            goto Portal;
+                        case 3:
+                            itemMaster.UpdateItem();
+                            goto Portal;
+                        case 4:
+                            itemMaster.ListAllItems();
+                            goto Portal;
+                        case 5:
+
+                            break;
+
+
+                        default:
+                            Console.WriteLine("Wrong Choise Try Again");
+                            goto Portal;
+
+
+                            ;
+
+                    }
+                    break;
+
+                case 2:
+
+                    CustomerMaster customerMaster = new CustomerMaster();
+
+                portal1:
+
+                    Console.WriteLine("********************************************Welcome to Customer Portal*****************************************");
+                    Console.WriteLine("Please Select one of the Options :\n");
+                    Console.WriteLine("\t\tPress 1 to Insert New Customer.");
+                    Console.WriteLine("\t\tPress 2 to Update Customer.");
+                    Console.WriteLine("\t\tPress 3 to Delete Customer.");
+                    Console.WriteLine("\t\tPress 4 to all Customer Table data.");
+                    Console.WriteLine("\t\tPress 5 to Exit");
+                    int choice3 = Convert.ToInt32(Console.ReadLine());
+                    switch (choice3)
+                    {
+                        case 1:
+                            customerMaster.AddCustomer();
+                            goto portal1;
+
+
+
+                        case 2:
+                            customerMaster.UpdateCustomer();
+                            goto Portal;
+
+
+
+
+                        case 3:
+                            customerMaster.DeleteCustomer();
+                            goto portal1;
+
+
+
+
+                        case 4:
+                            customerMaster.ListAllCustomers();
+                            goto portal1;
+
+
+
+                        case 5:
+                            break;
+                    }
+
+
+                    Console.ReadLine();
+
+
+                    break;
+
+                case 3:
+
+                    break;
+
+
+
+
+
+            }
+            //new Program().createtableitem();
+
+            //new Program().createtablecustomer();
+
+            //Console.ReadKey();
+        }
+
+        //#region creating itemmaster
+        //public void createtableitem()
+        //{
+
+        //    SqlConnection con = null;
+        //    try
+        //    {
+        //        // creating connection  
+        //        con = new SqlConnection("data source=hp-notebook; database=ordersdb; integrated security=true");
+        //        // writing sql query  
+        //        SqlCommand cm = new SqlCommand("create table itemmaster(itemname varchar(100) not null, Rate int not null, Qty int not null)", con);
+        //        //opening connection
+        //        con.Open();
+        //        // executing the sql query  
+        //        cm.ExecuteNonQuery();
+        //        // displaying a message  
+        //        Console.WriteLine("table itemmaster created successfully");
+
+
+        //    }
+        //    catch (Exception e)
+        //    {
+        //        Console.WriteLine("oops, something went wrong." + e);
+        //    }
+        //    // closing the connection  
+        //    finally
+        //    {
+        //        con.Close();
+        //    }
+        //}
+        //#endregion
+
+        //#region creatingcustomertable
+        //public void createtablecustomer()
+        //{
+        //    SqlConnection con = null;
+        //    try
+        //    {
+        //        con = new SqlConnection("data source=hp-notebook; database=ordersdb; integrated security=true");
+        //        SqlCommand cm1 = new SqlCommand("create table customermaster(firstname varchar(100) not null,lastname varchar(100) not null,email varchar(100) not null,  phone int not null)", con);
+        //        // opening connection  
+        //        con.Open();
+        //        // executing the sql query  
+        //        cm1.ExecuteNonQuery();
+        //        // displaying a message  
+        //        Console.WriteLine("table customermaster created successfully");
+
+        //    }
+        //    catch (Exception e)
+        //    {
+        //        Console.WriteLine("oops, something went wrong." + e);
+        //    }
+        //    // closing the connection  
+        //    finally
+        //    {
+        //        con.Close();
+        //    }
+        //}
+
+        //#endregion
+    }
+}
+    
+    
+
